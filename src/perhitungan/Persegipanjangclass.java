7ac0ab0b948b53b8b4d15bb60b5b5f986aafdfaf
rikasrirezeki11 <Rika.srirezeki@yahoo.com>
2015-10-25 17:09:20 +0700

    quis1java

---
+++ src/perhitungan/Persegipanjangclass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perhitungan;
+
+/**
+ *
+ * @author Owner
+ */
+public class Persegipanjangclass {
+  public int hasil;
+  public int Persegipanjangclass(int panjang,int lebar){
+      hasil=panjang*lebar;
+      return hasil;
+  }
+}
---
+++ src/perhitungan/persegiclass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perhitungan;
+
+/**
+ *
+ * @author Owner
+ */
+public class persegiclass {
+ public int hasil;
+ public int persegiclass (int sisi1,int sisi2){
+     hasil=sisi1*sisi2;
+     return hasil;
+ }
+}
